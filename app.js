6196dcb4d2f1bf9a07f8aba153ecfab599b20ea3
yashshekhar01 <yashshekhar2934@gmail.com>
2022-03-04 12:36:34 +0530

    Argument passing with Yargs

--- app.js
+++ app.js
@@ -1,14 +1,58 @@
 // const chalk = require('chalk')
 // const validator = require('validator')
-
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const command = process.argv[2]
+// const command = process.argv[2]
 
-console.log(process.argv)
+// Customize yargs version
+yargs.version('1.1.0')
 
-if (command === 'add') {
-    console.log('Adding note!')
-} else if (command === 'remove') {
-    console.log('Removing note!')
-}
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function(){
+        console.log('Adding a new note!')
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing the note!')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function(){
+        console.log('Listing out all notes!')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log('Reading a note!')
+    }
+})
+
+
+// add, remove, read, list
+
+// console.log(process.argv)
+
+console.log(yargs.argv)
+
+// if (command === 'add') {
+//     console.log('Adding note!')
+// } else if (command === 'remove') {
+//     console.log('Removing note!')
+// }
