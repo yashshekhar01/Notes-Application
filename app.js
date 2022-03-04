5d1b267863dfcda5005db87edf6f29f5a4bffd27
yashshekhar01 <yashshekhar2934@gmail.com>
2022-03-04 18:47:38 +0530

    Adding title, body by passing arguments

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // const chalk = require('chalk')
 // const validator = require('validator')
+const Argv = require('yargs')
 const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
@@ -12,8 +13,21 @@ yargs.version('1.1.0')
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function(){
-        console.log('Adding a new note!')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
     }
 })
 
@@ -49,7 +63,7 @@ yargs.command({
 
 // console.log(process.argv)
 
-console.log(yargs.argv)
+yargs.parse();
 
 // if (command === 'add') {
 //     console.log('Adding note!')
